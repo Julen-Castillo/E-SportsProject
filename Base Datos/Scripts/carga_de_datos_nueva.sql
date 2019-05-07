212438ed27b7c7dd679bf8aeec7a65335b1e4826
Adrian Danlos <adrianf1team@gmail.com>
2019-05-07 20:39:24 +0200

    update

--- Base Datos/Scripts/carga_de_datos_nueva.sql
+++ Base Datos/Scripts/carga_de_datos_nueva.sql
@@ -29,44 +29,44 @@ INSERT INTO JORNADA VALUES (1,SYSDATE,SYSDATE+5,1);
 /* Inserts de la tabla JUGADOR */
 
 --INSERTS JUGADORES FNATIC 1
-INSERT INTO JUGADOR VALUES (1,'tim','liposev','nemesis','midlane',1000,1);
-INSERT INTO JUGADOR VALUES (2,'gabriel','rau','bwipo','toplane',1000,1);
-INSERT INTO JUGADOR VALUES (3,'mads','brok-pedersen','broxah','jungler',1000,1);
-INSERT INTO JUGADOR VALUES (4,'zdravets','galabov','hylissang','support',1000,1);
-INSERT INTO JUGADOR VALUES (5,'martin','larsson','rekkles','adcarry',1000,1);
+INSERT INTO JUGADOR VALUES (1,'tim','liposev','nemesis','midlane',1000,1,1);
+INSERT INTO JUGADOR VALUES (2,'gabriel','rau','bwipo','toplane',1000,1,1);
+INSERT INTO JUGADOR VALUES (3,'mads','brok-pedersen','broxah','jungler',1000,1,1);
+INSERT INTO JUGADOR VALUES (4,'zdravets','galabov','hylissang','support',1000,1,1);
+INSERT INTO JUGADOR VALUES (5,'martin','larsson','rekkles','adcarry',1000,1,1);
 
 --INSERTS JUGADORES SK TELECOM T1 2
-INSERT INTO JUGADOR VALUES (6,'lee','sang-hyeok','faker','midlane',1000,2);
-INSERT INTO JUGADOR VALUES (7,'kim','jae-hee','crazy','toplane',1000,2);
-INSERT INTO JUGADOR VALUES (8,'kang','min-seung','haru','jungler',1000,2);
-INSERT INTO JUGADOR VALUES (9,'cho','se-hyeong','mata','support',1000,2);
-INSERT INTO JUGADOR VALUES (10,'park','jin-seong','teddy','adcarry',1000,2);
+INSERT INTO JUGADOR VALUES (6,'lee','sang-hyeok','faker','midlane',1000,1,2);
+INSERT INTO JUGADOR VALUES (7,'kim','jae-hee','crazy','toplane',1000,1,2);
+INSERT INTO JUGADOR VALUES (8,'kang','min-seung','haru','jungler',1000,1,2);
+INSERT INTO JUGADOR VALUES (9,'cho','se-hyeong','mata','support',1000,1,2);
+INSERT INTO JUGADOR VALUES (10,'park','jin-seong','teddy','adcarry',1000,1,2);
 
 --INSERTS JUGADORES CLOUD9 3
 
-INSERT INTO JUGADOR VALUES (11,'yasin','dinçer','nisqy','midlane',1000,3);
-INSERT INTO JUGADOR VALUES (12,'eric','rtichie','licorice','toplane',1000,3);
-INSERT INTO JUGADOR VALUES (13,'robert','huang','blaber','jungler',1000,3);
-INSERT INTO JUGADOR VALUES (14,'tristan','stidam','zeyzal','support',1000,3);
-INSERT INTO JUGADOR VALUES (15,'zachary','scuderi','sneaky','adcarry',1000,3);
+INSERT INTO JUGADOR VALUES (11,'yasin','dinçer','nisqy','midlane',1000,1,3);
+INSERT INTO JUGADOR VALUES (12,'eric','rtichie','licorice','toplane',1000,1,3);
+INSERT INTO JUGADOR VALUES (13,'robert','huang','blaber','jungler',1000,1,3);
+INSERT INTO JUGADOR VALUES (14,'tristan','stidam','zeyzal','support',1000,1,3);
+INSERT INTO JUGADOR VALUES (15,'zachary','scuderi','sneaky','adcarry',1000,1,3);
 
 --INSERTS JUGADORES TSM 4 
 
-INSERT INTO JUGADOR VALUES (16,'soren','bjerg','bjergsen','midlane',1000,4);
-INSERT INTO JUGADOR VALUES (17,'sergen','çelik','broken blade','toplane',1000,4);
-INSERT INTO JUGADOR VALUES (18,'matthew','higginotham','akaadian','jungler',1000,4);
-INSERT INTO JUGADOR VALUES (19,'andy','ta','smoothie','support',1000,4);
-INSERT INTO JUGADOR VALUES (20,'jesper','svenningsen','zven','adcarry',1000,4);
+INSERT INTO JUGADOR VALUES (16,'soren','bjerg','bjergsen','midlane',1000,1,4);
+INSERT INTO JUGADOR VALUES (17,'sergen','çelik','broken blade','toplane',1000,1,4);
+INSERT INTO JUGADOR VALUES (18,'matthew','higginotham','akaadian','jungler',1000,1,4);
+INSERT INTO JUGADOR VALUES (19,'andy','ta','smoothie','support',1000,1,4);
+INSERT INTO JUGADOR VALUES (20,'jesper','svenningsen','zven','adcarry',1000,1,4);
 
 --INSERTS JUGADORES THUNDERX3 BASKONIA 5
 
-INSERT INTO JUGADOR VALUES (21,'danny','le comite','dan dan','toplane',1000,5);
-INSERT INTO JUGADOR VALUES (22,'sergio','martin','diamond','midlane',1000,5);
-INSERT INTO JUGADOR VALUES (23,'ignacio','garcia','itsi','jungler',1000,5);
-INSERT INTO JUGADOR VALUES (24,'adrain','moldes','homi','support',1000,5);
-INSERT INTO JUGADOR VALUES (25,'alejandro','gonzalez','darkside','adcarry',1000,5);
+INSERT INTO JUGADOR VALUES (21,'danny','le comite','dan dan','toplane',1000,1,5);
+INSERT INTO JUGADOR VALUES (22,'sergio','martin','diamond','midlane',1000,1,5);
+INSERT INTO JUGADOR VALUES (23,'ignacio','garcia','itsi','jungler',1000,1,5);
+INSERT INTO JUGADOR VALUES (24,'adrain','moldes','homi','support',1000,1,5);
+INSERT INTO JUGADOR VALUES (25,'alejandro','gonzalez','darkside','adcarry',1000,1,5);
 
 --INSERT JUGADORES MAMECORP 6
-INSERT INTO JUGADOR VALUES (26,'julen','castillo','castiskate','midlane',3000,6);
-INSERT INTO JUGADOR VALUES (27,'adrian','danlos','godskater','toplane',3000,6);
-INSERT INTO JUGADOR VALUES (28,'julen','prieto','peto','jungler',3000,6);
+INSERT INTO JUGADOR VALUES (26,'julen','castillo','castiskate','midlane',3000,1,6);
+INSERT INTO JUGADOR VALUES (27,'adrian','danlos','godskater','toplane',3000,1,6);
+INSERT INTO JUGADOR VALUES (28,'julen','prieto','peto','jungler',3000,1,6);
