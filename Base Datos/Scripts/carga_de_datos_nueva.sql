4450a1836772fc36f2a41ead492e85334353c286
Julen Prieto <prietojulen@gmail.com>
2019-05-10 10:27:16 +0200

    carga de datos con secuencias

--- Base Datos/Scripts/carga_de_datos_nueva.sql
+++ Base Datos/Scripts/carga_de_datos_nueva.sql
@@ -2,80 +2,137 @@
 
 /* Inserts de la tabla EQUIPO */
 
-INSERT INTO EQUIPO VALUES (1,'fnatic',170000,0);
-INSERT INTO EQUIPO VALUES (2,'sk telecom t1',180000,0);
-INSERT INTO EQUIPO VALUES (3,'cloud9',160000,0);
-INSERT INTO EQUIPO VALUES (4,'tsm',150000,0);
-INSERT INTO EQUIPO VALUES (5,'thunderx3 baskonia',50000,0);
-INSERT INTO EQUIPO VALUES (6,'mamecorp',200000,5);
+DROP SEQUENCE seq_equipo;
 
+  CREATE SEQUENCE seq_equipo
+  	START WITH 10
+  	INCREMENT BY 10
+  	MAXVALUE 2000
+  	MINVALUE 10
+  	NOCYCLE;
+
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'fnatic',170000,0);
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'sk telecom t1',180000,0);
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'cloud9',160000,0);
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'tsm',150000,0);
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'thunderx3 baskonia',50000,0);
+INSERT INTO EQUIPO VALUES (seq_equipo.nextval,'mamecorp',200000,5);
+
+commit;
 /* Inserts de la tabla JUGADOR */
 
+DROP SEQUENCE seq_jugador;
+
+ CREATE SEQUENCE seq_jugador
+  	START WITH 1
+  	INCREMENT BY 1
+  	MAXVALUE 9999
+  	MINVALUE 1
+  	NOCYCLE;
+
 --INSERTS JUGADORES FNATIC 1
-INSERT INTO JUGADOR VALUES (1,'tim','liposev','nemesis','midlane',1000,1,1);
-INSERT INTO JUGADOR VALUES (2,'gabriel','rau','bwipo','toplane',1000,1,1);
-INSERT INTO JUGADOR VALUES (3,'mads','brok-pedersen','broxah','jungler',1000,1,1);
-INSERT INTO JUGADOR VALUES (4,'zdravets','galabov','hylissang','support',1000,1,1);
-INSERT INTO JUGADOR VALUES (5,'martin','larsson','rekkles','adcarry',1000,1,1);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'tim','liposev','nemesis','midlane',1000,1,10);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'gabriel','rau','bwipo','toplane',1000,1,10);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'mads','brok-pedersen','broxah','jungler',1000,1,10);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'zdravets','galabov','hylissang','support',1000,1,10);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'martin','larsson','rekkles','adcarry',1000,1,10);
 
 --INSERTS JUGADORES SK TELECOM T1 2
-INSERT INTO JUGADOR VALUES (6,'lee','sang-hyeok','faker','midlane',1000,1,2);
-INSERT INTO JUGADOR VALUES (7,'kim','jae-hee','crazy','toplane',1000,1,2);
-INSERT INTO JUGADOR VALUES (8,'kang','min-seung','haru','jungler',1000,1,2);
-INSERT INTO JUGADOR VALUES (9,'cho','se-hyeong','mata','support',1000,1,2);
-INSERT INTO JUGADOR VALUES (10,'park','jin-seong','teddy','adcarry',1000,1,2);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'lee','sang-hyeok','faker','midlane',1000,1,20);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'kim','jae-hee','crazy','toplane',1000,1,20);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'kang','min-seung','haru','jungler',1000,1,20);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'cho','se-hyeong','mata','support',1000,1,20);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'park','jin-seong','teddy','adcarry',1000,1,20);
 
 --INSERTS JUGADORES CLOUD9 3
 
-INSERT INTO JUGADOR VALUES (11,'yasin','dinçer','nisqy','midlane',1000,1,3);
-INSERT INTO JUGADOR VALUES (12,'eric','rtichie','licorice','toplane',1000,1,3);
-INSERT INTO JUGADOR VALUES (13,'robert','huang','blaber','jungler',1000,1,3);
-INSERT INTO JUGADOR VALUES (14,'tristan','stidam','zeyzal','support',1000,1,3);
-INSERT INTO JUGADOR VALUES (15,'zachary','scuderi','sneaky','adcarry',1000,1,3);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'yasin','dinçer','nisqy','midlane',1000,1,30);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'eric','rtichie','licorice','toplane',1000,1,30);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'robert','huang','blaber','jungler',1000,1,30);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'tristan','stidam','zeyzal','support',1000,1,30);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'zachary','scuderi','sneaky','adcarry',1000,1,30);
 
 --INSERTS JUGADORES TSM 4 
 
-INSERT INTO JUGADOR VALUES (16,'soren','bjerg','bjergsen','midlane',1000,1,4);
-INSERT INTO JUGADOR VALUES (17,'sergen','çelik','broken blade','toplane',1000,1,4);
-INSERT INTO JUGADOR VALUES (18,'matthew','higginotham','akaadian','jungler',1000,1,4);
-INSERT INTO JUGADOR VALUES (19,'andy','ta','smoothie','support',1000,1,4);
-INSERT INTO JUGADOR VALUES (20,'jesper','svenningsen','zven','adcarry',1000,1,4);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'soren','bjerg','bjergsen','midlane',1000,1,40);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'sergen','çelik','broken blade','toplane',1000,1,40);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'matthew','higginotham','akaadian','jungler',1000,1,40);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'andy','ta','smoothie','support',1000,1,40);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'jesper','svenningsen','zven','adcarry',1000,1,40);
 
 --INSERTS JUGADORES THUNDERX3 BASKONIA 5
 
-INSERT INTO JUGADOR VALUES (21,'danny','le comite','dan dan','toplane',1000,1,5);
-INSERT INTO JUGADOR VALUES (22,'sergio','martin','diamond','midlane',1000,1,5);
-INSERT INTO JUGADOR VALUES (23,'ignacio','garcia','itsi','jungler',1000,1,5);
-INSERT INTO JUGADOR VALUES (24,'adrain','moldes','homi','support',1000,1,5);
-INSERT INTO JUGADOR VALUES (25,'alejandro','gonzalez','darkside','adcarry',1000,1,5);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'danny','le comite','dan dan','toplane',1000,1,50);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'sergio','martin','diamond','midlane',1000,1,50);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'ignacio','garcia','itsi','jungler',1000,1,50);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'adrain','moldes','homi','support',1000,1,50);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'alejandro','gonzalez','darkside','adcarry',1000,1,50);
 
 --INSERT JUGADORES MAMECORP 6
-INSERT INTO JUGADOR VALUES (26,'julen','castillo','castiskate','midlane',3000,1,6);
-INSERT INTO JUGADOR VALUES (27,'adrian','danlos','godskater','toplane',3000,1,6);
-INSERT INTO JUGADOR VALUES (28,'julen','prieto','peto','jungler',3000,1,6);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'julen','castillo','castiskate','midlane',3000,1,60);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'adrian','danlos','godskater','toplane',3000,1,60);
+INSERT INTO JUGADOR VALUES (seq_jugador.nextval,'julen','prieto','peto','jungler',3000,1,60);
+
+commit;
 
 /*Insert de la tabla PRESIDENTE*/
 
-INSERT INTO PRESIDENTE VALUES(1,'sam','mathews',1);
-INSERT INTO PRESIDENTE VALUES(2,'sung','min ha',2);
-INSERT INTO PRESIDENTE VALUES(3,'jack','etienne',3);
-INSERT INTO PRESIDENTE VALUES(4,'andy','dinh',4);
-INSERT INTO PRESIDENTE VALUES(5,'jose antonio','querejeta',5);
-INSERT INTO PRESIDENTE VALUES(6,'eric','con c',6);
+DROP SEQUENCE seq_presidente;
 
+   CREATE SEQUENCE seq_presidente
+  	START WITH 101
+  	INCREMENT BY 1
+  	MAXVALUE 99999
+  	MINVALUE 1
+  	NOCYCLE;
 
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'sam','mathews',10);
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'sung','min ha',20);
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'jack','etienne',30);
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'andy','dinh',40);
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'jose antonio','querejeta',50);
+INSERT INTO PRESIDENTE VALUES(seq_presidente.nextval,'eric','con c',60);
+
+commit;
 
 /*Insert de la tabla LIGA*/
 
-INSERT INTO LIGA VALUES (1,'MameCorp league', SYSDATE,SYSDATE,1);
+DROP SEQUENCE seq_liga;
+
+ CREATE SEQUENCE seq_liga
+  	START WITH 1
+  	INCREMENT BY 1
+  	MAXVALUE 99
+  	MINVALUE 1
+  	NOCYCLE;
+
+
+INSERT INTO LIGA VALUES (seq_liga.nextval,'MameCorp league', SYSDATE,SYSDATE,1);
+
+commit;
 
 /* Inserts de la tabla JORNADA */ 
 
-INSERT INTO JORNADA VALUES (1,SYSDATE,SYSDATE+7,1);
+DROP SEQUENCE seq_jornada;
+
+  CREATE  SEQUENCE seq_jornada
+  	START WITH 1
+  	INCREMENT BY 1
+  	MAXVALUE 9999
+  	MINVALUE 1
+  	NOCYCLE;
+
+
+INSERT INTO JORNADA VALUES (seq_jornada.nextval,SYSDATE,SYSDATE+7,1);
+
+commit;
 
 /* Inserts de la tabla PARTIDO */
 
-INSERT INTO PARTIDO VALUES (1,1,2,1,0,SYSDATE,SYSDATE + 2595/86400,15,20,230000,250000);
+
+INSERT INTO PARTIDO VALUES (10,1,20,10,0,SYSDATE,SYSDATE + 2595/86400,15,20,230000,250000);
+
+commit;
 
 
 /*ALTERAR EL SYSDATE PARA QUE MUESTRE HORAS24:MINUTOS:SEGUNDOS  */
@@ -92,7 +149,16 @@ ALTER SESSION SET nls_date_format = 'dd-mm-yyyy hh24:mi:ss';
 --SYSDATE + 2595/86400
 
 /* Inserts de la tabla USUARIO */
+DROP SEQUENCE seq_usuario;
+
+  CREATE SEQUENCE seq_usuario
+  	START WITH 1000
+  	INCREMENT BY 100
+  	MAXVALUE 9900
+  	MINVALUE 1
+  	NOCYCLE;
 
 INSERT INTO USUARIO VALUES(1,'mamecorp','mamecorp','admin');
 INSERT INTO USUARIO VALUES(2,'user','user','user');
 
+commit;
