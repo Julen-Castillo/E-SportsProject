9d5e6d84aed9b99ccf7f8b08ae5d1122f5e60581
Julen <julen.castillo@ikasle.egibide.org>
2019-05-15 10:21:16 +0200

    Base de datos

---
+++ Base Datos/Scripts/Paquete/PAQUETE BUENO.sql
@@ -0,0 +1,89 @@
+/*CABECERA DEL PAQUETE */
+
+CREATE OR REPLACE PACKAGE paquete_mamecorp AS
+	FUNCTION FUNCTION_COUNT_JUG_EQUIPO (ID_EQUIPO IN EQUIPO.ID_EQUIPO%TYPE)
+		RETURN NUMBER;
+        
+	PROCEDURE generar_calendario;
+    CURSOR c_info_equipo is  SELECT E.ID_EQUIPO, E.NOMBRE, E.PRESUPUESTO, E.PUNTOS, P.ID_PRESIDENTE, P.NOMBRE AS PRESIDENTE
+		    FROM EQUIPO E, PRESIDENTE P
+		    WHERE P.EQUIPO_ID_EQUIPO(+) = E.ID_EQUIPO;  
+            
+	PROCEDURE procedimiento_info_equipo; 
+END paquete_mamecorp;
+
+
+CREATE OR REPLACE PACKAGE BODY paquete_mamecorp AS
+
+	FUNCTION FUNCTION_COUNT_JUG_EQUIPO (ID_EQUIPO IN EQUIPO.ID_EQUIPO%TYPE)
+	RETURN NUMBER
+		AS
+		NUM_JUGADORES NUMBER;
+        DECLARE 
+        e_equipo_not_found EXCEPTION;
+		BEGIN
+		    SELECT COUNT(*) INTO NUM_JUGADORES 
+		    FROM JUGADOR
+		    WHERE Equipo_id_equipo = id_equipo;
+            IF SQL%NOTFOUND THEN
+            RAISE e_equipo_not_found;
+		RETURN(NUM_JUGADORES);
+    EXCEPTION 
+        WHEN e_equipo_not_found THEN
+            RAISE_APPLICATION_ERROR(-20021, 'El equipo no existe');
+        WHEN VALUE_ERROR THEN
+            RAISE_APPLICATION_ERROR(-06502, 'El tipo de dato a introducir es un numero de equipo');
+	END FUNCTION_COUNT_JUG_EQUIPO;
+
+
+	
+
+	PROCEDURE generar_calendario IS
+		v_dias_next_jornada NUMBER := 0; --variable que incrementa en 7 tras cada jornada. La utilizamos para establecer la fecha de la siguiente jornada.
+		v_num_jornadas NUMBER := 0; --contador de jornadas
+		v_increment_id_jornada NUMBER := 1; --id jornada autoincremental
+		v_jornadas_totales NUMBER := 10;
+		v_f_inicio_liga DATE;
+		v_f_fin_liga LIGA.FECHA_FIN%TYPE;
+		BEGIN
+		    DELETE FROM JORNADA; --eliminamos todas las jornadas antes de crear el calendario
+		    DELETE FROM LIGA; --eliminamos las ligas antes de crear el calendario
+		    INSERT INTO LIGA VALUES (1, 'MAMECORPLEAGUE', SYSDATE, NULL, 0); --Creamos la liga sin fecha fin
+		    SELECT FECHA_INICIO INTO v_f_inicio_liga 
+		    FROM liga
+		    WHERE id_liga = 1;
+		    WHILE(v_num_jornadas < v_jornadas_totales) --Queremos 10 iteraciones dado que queremos 10 jornadas
+		        LOOP
+		            INSERT INTO JORNADA VALUES(v_increment_id_jornada, v_f_inicio_liga  + v_dias_next_jornada, v_f_inicio_liga  + 2 + v_dias_next_jornada, 1);
+		            v_dias_next_jornada := v_dias_next_jornada + 7;
+		            v_num_jornadas := v_num_jornadas + 1;
+		            v_increment_id_jornada := v_increment_id_jornada + 1;
+		        END LOOP;  
+		    SELECT FECHA_FIN 
+		    INTO v_f_fin_liga
+		    FROM JORNADA
+		    WHERE ID_JORNADA = (SELECT MAX(ID_JORNADA) FROM JORNADA);
+		    UPDATE LIGA 
+		    SET FECHA_FIN = v_f_fin_liga
+		    WHERE ID_LIGA = 1;
+	END generar_calendario;
+
+   
+	PROCEDURE procedimiento_info_equipo IS
+		CURSOR c_info_equipo IS
+		    SELECT E.ID_EQUIPO, E.NOMBRE, E.PRESUPUESTO, E.PUNTOS, P.ID_PRESIDENTE, P.NOMBRE AS PRESIDENTE
+		    FROM EQUIPO E, PRESIDENTE P
+		    WHERE P.EQUIPO_ID_EQUIPO(+) = E.ID_EQUIPO;   
+		v_info c_info_equipo%ROWTYPE;
+		v_num_jugadores NUMBER;
+		BEGIN
+		    OPEN c_info_equipo;
+		    LOOP
+		    FETCH c_info_equipo INTO v_info; 
+		        v_num_jugadores := FUNCTION_COUNT_JUG_EQUIPO(v_info.ID_EQUIPO);
+		        DBMS_OUTPUT.PUT_LINE('ID EQUIPO: ' || v_info.ID_EQUIPO || ' / NOMBRE: ' || v_info.NOMBRE || ' / PRESUPUESTO: ' || v_info.PRESUPUESTO || ' / PUNTOS: ' || v_info.PUNTOS || ' / NUM JUGADORES: ' || v_num_jugadores || ' / ID_PRESIDENTE: ' || NVL(TO_CHAR(v_info.ID_PRESIDENTE), 'No tiene presidente') || ' / NOMBRE: ' || NVL(v_info.PRESIDENTE, 'No tiene presidente'));
+		    EXIT WHEN c_info_equipo%notfound;
+		    END LOOP;
+		    CLOSE c_info_equipo;
+		END;
+	END paquete_mamecorp;
--- Base Datos/Scripts/Script creacion e inserts/carga_de_datos_sin_seq.sql
+++ Base Datos/Scripts/Script creacion e inserts/carga_de_datos_sin_seq.sql
@@ -5,12 +5,12 @@
 
 /* Inserts de la tabla EQUIPO */
 
-INSERT INTO EQUIPO VALUES (1,'fnatic',170000,0);
-INSERT INTO EQUIPO VALUES (2,'sk telecom t1',180000,0);
-INSERT INTO EQUIPO VALUES (3,'cloud9',160000,0);
-INSERT INTO EQUIPO VALUES (4,'tsm',150000,0);
-INSERT INTO EQUIPO VALUES (5,'thunderx3 baskonia',50000,0);
-INSERT INTO EQUIPO VALUES (6,'mamecorp',200000,5);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('fnatic',170000,0);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('sk telecom t1',180000,0);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('cloud9',160000,0);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('tsm',150000,0);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('thunderx3 baskonia',50000,0);
+INSERT INTO EQUIPO (nombre,presupuesto,puntos)  VALUES ('mamecorp',200000,5);
 
 /* Inserts de la tabla EQUIPOPARTIDO */
 
@@ -29,44 +29,66 @@ INSERT INTO EQUIPO VALUES (6,'mamecorp',200000,5);
 /* Inserts de la tabla JUGADOR */
 
 --INSERTS JUGADORES FNATIC 1
-INSERT INTO JUGADOR VALUES (1,'tim','liposev','nemesis','midlane',1000,1,1);
-INSERT INTO JUGADOR VALUES (2,'gabriel','rau','bwipo','toplane',1000,1,1);
-INSERT INTO JUGADOR VALUES (3,'mads','brok-pedersen','broxah','jungler',1000,1,1);
-INSERT INTO JUGADOR VALUES (4,'zdravets','galabov','hylissang','support',1000,1,1);
-INSERT INTO JUGADOR VALUES (5,'martin','larsson','rekkles','adcarry',1000,1,1);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('tim','liposev','nemesis','midlane',1000,1,1);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('gabriel','rau','bwipo','toplane',1000,1,1);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('mads','brok-pedersen','broxah','jungler',1000,1,1);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('zdravets','galabov','hylissang','support',1000,1,1);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo)VALUES ('martin','larsson','rekkles','adcarry',1000,1,1);
 
 --INSERTS JUGADORES SK TELECOM T1 2
-INSERT INTO JUGADOR VALUES (6,'lee','sang-hyeok','faker','midlane',1000,1,2);
-INSERT INTO JUGADOR VALUES (7,'kim','jae-hee','crazy','toplane',1000,1,2);
-INSERT INTO JUGADOR VALUES (8,'kang','min-seung','haru','jungler',1000,1,2);
-INSERT INTO JUGADOR VALUES (9,'cho','se-hyeong','mata','support',1000,1,2);
-INSERT INTO JUGADOR VALUES (10,'park','jin-seong','teddy','adcarry',1000,1,2);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('lee','sang-hyeok','faker','midlane',1000,1,2);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo)VALUES ('kim','jae-hee','crazy','toplane',1000,1,2);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('kang','min-seung','haru','jungler',1000,1,2);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('cho','se-hyeong','mata','support',1000,1,2);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('park','jin-seong','teddy','adcarry',1000,1,2);
 
 --INSERTS JUGADORES CLOUD9 3
 
-INSERT INTO JUGADOR VALUES (11,'yasin','dinçer','nisqy','midlane',1000,1,3);
-INSERT INTO JUGADOR VALUES (12,'eric','rtichie','licorice','toplane',1000,1,3);
-INSERT INTO JUGADOR VALUES (13,'robert','huang','blaber','jungler',1000,1,3);
-INSERT INTO JUGADOR VALUES (14,'tristan','stidam','zeyzal','support',1000,1,3);
-INSERT INTO JUGADOR VALUES (15,'zachary','scuderi','sneaky','adcarry',1000,1,3);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('yasin','dinçer','nisqy','midlane',1000,1,3);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('eric','rtichie','licorice','toplane',1000,1,3);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('robert','huang','blaber','jungler',1000,1,3);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('tristan','stidam','zeyzal','support',1000,1,3);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('zachary','scuderi','sneaky','adcarry',1000,1,3);
 
 --INSERTS JUGADORES TSM 4 
 
-INSERT INTO JUGADOR VALUES (16,'soren','bjerg','bjergsen','midlane',1000,1,4);
-INSERT INTO JUGADOR VALUES (17,'sergen','çelik','broken blade','toplane',1000,1,4);
-INSERT INTO JUGADOR VALUES (18,'matthew','higginotham','akaadian','jungler',1000,1,4);
-INSERT INTO JUGADOR VALUES (19,'andy','ta','smoothie','support',1000,1,4);
-INSERT INTO JUGADOR VALUES (20,'jesper','svenningsen','zven','adcarry',1000,1,4);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('soren','bjerg','bjergsen','midlane',1000,1,4);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('sergen','çelik','broken blade','toplane',1000,1,4);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('matthew','higginotham','akaadian','jungler',1000,1,4);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('andy','ta','smoothie','support',1000,1,4);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('jesper','svenningsen','zven','adcarry',1000,1,4);
 
 --INSERTS JUGADORES THUNDERX3 BASKONIA 5
 
-INSERT INTO JUGADOR VALUES (21,'danny','le comite','dan dan','toplane',1000,1,5);
-INSERT INTO JUGADOR VALUES (22,'sergio','martin','diamond','midlane',1000,1,5);
-INSERT INTO JUGADOR VALUES (23,'ignacio','garcia','itsi','jungler',1000,1,5);
-INSERT INTO JUGADOR VALUES (24,'adrain','moldes','homi','support',1000,1,5);
-INSERT INTO JUGADOR VALUES (25,'alejandro','gonzalez','darkside','adcarry',1000,1,5);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('danny','le comite','dan dan','toplane',1000,1,5);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('sergio','martin','diamond','midlane',1000,1,5);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('ignacio','garcia','itsi','jungler',1000,1,5);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('adrain','moldes','homi','support',1000,1,5);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('alejandro','gonzalez','darkside','adcarry',1000,1,5);
 
 --INSERT JUGADORES MAMECORP 6
-INSERT INTO JUGADOR VALUES (26,'julen','castillo','castiskate','midlane',3000,1,6);
-INSERT INTO JUGADOR VALUES (27,'adrian','danlos','godskater','toplane',3000,1,6);
-INSERT INTO JUGADOR VALUES (28,'julen','prieto','peto','jungler',3000,1,6);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('julen','castillo','castiskate','midlane',3000,1,6);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('adrian','danlos','godskater','toplane',3000,1,6);
+INSERT INTO JUGADOR(nombre,apellido,nickname,posicion,sueldo,titularidad,equipo_id_equipo) VALUES ('julen','prieto','peto','jungler',3000,1,6);
+
+
+--INSERT PRESIDENTES
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('sam','mathews',1);
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('sung','min ha',2);
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('jack','etienne',3);
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('andy','dinh',4);
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('jose antonio','querejeta',5);
+INSERT INTO PRESIDENTE (nombre,apellido,equipo_id_equipo)VALUES('eric','con c',6);
+
+
+--INSERT LIGA
+INSERT INTO LIGA (nombre,fecha_inicio,fecha_fin,en_curso) VALUES ('MameCorp league', SYSDATE,SYSDATE,1);
+
+--INSERT JORNADA 
+INSERT INTO JORNADA (FECHA_INICIO,FECHA_FIN,LIGA_ID_LIGA) VALUES (SYSDATE,SYSDATE+7,1);
+
+--INSERT USUARIOS
+
+INSERT INTO USUARIO(nombre,password,tipo) VALUES('mamecorp','mamecorp','admin');
+INSERT INTO USUARIO(nombre,password,tipo) VALUES('user','user','user');
+
