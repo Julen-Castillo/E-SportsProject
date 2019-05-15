d49a2df021adb5cee2642bc9da9325187e14de02
Julen <julen.castillo@ikasle.egibide.org>
2019-05-15 14:06:44 +0200

    connection

---
+++ Programacion/MameCorp League/src/modeloDB/GenericoDB.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloDB;
+
+import java.sql.*;
+import javax.swing.SwingConstants;
+
+
+public class GenericoDB {
+    
+    private static  Connection con;
+    
+
+
+public static void conectar(){
+    try {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        String url="jdbc:oracle:thin:@SrvOracle:1521:orcl";
+        con = DriverManager.getConnection(url,"eqdaw01","eqdaw01");
+      
+
+    } catch (Exception e){
+        System.out.println("Problemas con la base de datos, no es culpa nuestra, es de egibide");
+        System.exit(0);
+
+    }
+}
+
+public static Connection getCon(){
+    return con;
+}
+        
+}
