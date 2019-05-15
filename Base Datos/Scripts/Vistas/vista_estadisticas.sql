bee2907f98c0e6eab4af0402f2ff6a0ceddc92f1
Julen <julen.castillo@ikasle.egibide.org>
2019-05-15 10:45:57 +0200

    Exodus

--- Base Datos/Scripts/Vistas/vista_estadisticas.sql
+++
@@ -1,8 +0,0 @@
---Creamos una vista para mostrar las estadisticas que han surjido del partido.
-
-CREATE OR REPLACE VIEW vista_estadisticas AS
-    SELECT FECHA_INICIO "HORA INICIO", ROUND(FECHA_FIN - FECHA_INICIO, 2)*1440 "TIEMPO PARTIDA", KILLS_EQUIPO_LOCAL, KILLS_EQUIPO_VISITANTE, ORO_EQUIPO_LOCAL, ORO_EQUIPO_VISITANTE
-    FROM PARTIDO;
-    
-
-    
